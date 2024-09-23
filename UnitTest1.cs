046269fc2061c9cd0d4661c3c24ad25e63df28a6
M-Agi <agnes.macsotai@gmail.com>
2024-09-23 15:15:31 +0200

    Create unit test project

---
+++ UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Betsson.OnlineWallets.UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Console.WriteLine("Hello World");
+        Assert.AreEqual(0, 1);
+    }
+}
