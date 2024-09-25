f63d60c63f942ed1ba20470ae9048502455e87c9
M-Agi <agnes.macsotai@gmail.com>
2024-09-25 13:58:47 +0200

    Added daposit tests with invalid value

--- Betsson.OnlineWallets.Data.IntegrationTests/UnitTest1.cs
+++ Betsson.OnlineWallets.Data.IntegrationTests/UnitTest1.cs
@@ -25,8 +25,7 @@ public class ApiTests
     }
 
     public static decimal Deposit(decimal amount)
-    {
-        
+    {        
         var client = new RestClient( new RestClientOptions {
             BaseUrl = new Uri("http://localhost:8080")
         } ); 
@@ -40,6 +39,21 @@ public class ApiTests
         Assert.AreEqual( response.StatusDescription, "OK");
         var json = JsonConvert.DeserializeObject<Balance>(response.Content);
         return json.Amount;
+    }
+
+    public static void DepositExpectToFail(decimal amount)
+    {        
+        var client = new RestClient( new RestClientOptions {
+            BaseUrl = new Uri("http://localhost:8080")
+        } ); 
+        var request = new RestRequest( "/onlinewallet/deposit", Method.Post);
+
+        request.AddJsonBody( "{ \"amount\": " + amount + "  }" );
+    
+        var response = client.Execute( request );
+        
+        Assert.AreEqual( response.StatusCode, System.Net.HttpStatusCode.BadRequest);
+        Assert.AreEqual( response.StatusDescription, "Bad Request");
     }
 
     public static decimal Withdraw(decimal amount)
@@ -108,6 +122,50 @@ public class ApiTests
         ClearBalance();
         var amount = Deposit(99999999999999999999999m);
         Assert.AreEqual( amount,  99999999999999999999999m);
+        ClearBalance();
+    }
+
+    [TestMethod]
+    public void TestDepositInvalidValue()
+    {               
+        ClearBalance();
+        DepositExpectToFail(-50);
+        var amount = GetBalance();
+        Assert.AreEqual( amount,  0);
+        ClearBalance();
+    }
+
+    [TestMethod]
+    public void TestDepositFirstValidThanInvalidValue()
+    {               
+        ClearBalance();
+
+        var amount = Deposit(999999m);
+        Assert.AreEqual( amount,  999999m);
+
+        DepositExpectToFail(-50);
+
+        var balance = GetBalance();
+        Assert.AreEqual( balance,  999999m);
+        ClearBalance();
+    }
+
+    [TestMethod]
+    public void TestDepositFirstValidThanInvalidThanValidValue()
+    {               
+        ClearBalance();
+
+        var amount = Deposit(9);
+        Assert.AreEqual( amount,  9);
+
+        DepositExpectToFail(-50);
+
+        var balance = GetBalance();
+        Assert.AreEqual( balance,  9);
+
+        var amount2 = Deposit(10);
+        Assert.AreEqual( amount2,  9 + 10);
+
         ClearBalance();
     }
 }
